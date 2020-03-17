a3a74c0594db6ee9b5227595930f6b1fcdd088a7
Sebastiandg7 <sebastiandg7@gmail.com>
2020-03-16 21:54:07 -0500

    feat(input-components): add basic layout & interaction

--- libs/react-coding-test/feature-input-components/src/lib/react-coding-test-feature-input-components.tsx
+++ libs/react-coding-test/feature-input-components/src/lib/react-coding-test-feature-input-components.tsx
@@ -1,20 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { ToggleSwitch } from '@tf-test/react/ui';
 
 import styled from 'styled-components';
+import FocusableInput from './components/focusble-input/focusable-input';
 
 /* eslint-disable-next-line */
 export interface ReactCodingTestFeatureInputComponentsProps {}
 
 const StyledReactCodingTestFeatureInputComponents = styled.div`
-  color: pink;
+  .settings,
+  .input-controls,
+  .result {
+    margin: 3em 0;
+  }
+
+  .settings {
+    display: flex;
+  }
+
+  .setttings-property-lbl {
+    display: block;
+    margin-bottom: 0.5em;
+  }
+
+  .input-controls {
+    margin: 3em 0;
+  }
+
+  .lbl {
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+
+  .btn {
+    background: #143055;
+    color: #fff;
+    padding: 0.6em 1.2em;
+    min-width: 8.62em;
+    text-align: center;
+    border-radius: 5px;
+  }
+
+  .btn.gray {
+    background: #757575;
+  }
 `;
 
 export const ReactCodingTestFeatureInputComponents = (
   props: ReactCodingTestFeatureInputComponentsProps
 ) => {
+  const [inputRendered, setInputRendered] = useState(true);
+
   return (
     <StyledReactCodingTestFeatureInputComponents>
-      <h1>Welcome to react-coding-test-feature-input-components component!</h1>
+      <div className="settings">
+        <div className="settings-col">
+          <div className="setttings-property">
+            <label htmlFor="" className="lbl setttings-property-lbl">
+              Input focused
+            </label>
+            <div className="setttings-property-value">
+              <ToggleSwitch rounded={true} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="input-controls">
+        <button
+          className={`btn ${inputRendered ? 'gray' : ''}`}
+          onClick={() => setInputRendered(!inputRendered)}
+        >{`${inputRendered ? 'Remove' : 'Render'} input`}</button>
+      </div>
+
+      <div className="result">{inputRendered ? <FocusableInput /> : <></>}</div>
     </StyledReactCodingTestFeatureInputComponents>
   );
 };
