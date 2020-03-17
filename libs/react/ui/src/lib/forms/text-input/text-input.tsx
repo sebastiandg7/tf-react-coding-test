65c2d8c4819436c12a93001355216b7336e5ec28
Sebastiandg7 <sebastiandg7@gmail.com>
2020-03-16 22:06:07 -0500

    feat(input-components): style text input component

--- libs/react/ui/src/lib/forms/text-input/text-input.tsx
+++ libs/react/ui/src/lib/forms/text-input/text-input.tsx
@@ -6,13 +6,27 @@ import styled from 'styled-components';
 export interface TextInputProps {}
 
 const StyledTextInput = styled.div`
-  color: pink;
+  .txt-input {
+    color: #333;
+    font-size: 1.2rem;
+    padding: 0.75rem 1rem;
+    border-radius: 0.2rem;
+    background-color: rgb(255, 255, 255);
+    border: none;
+    display: block;
+    border: 0.1rem solid #143055;
+  }
 `;
 
 export const TextInput = (props: TextInputProps) => {
   return (
     <StyledTextInput>
-      <h1>Welcome to text-input component!</h1>
+      <input
+        type="text"
+        className="txt-input"
+        id="name"
+        required={true}
+      />
     </StyledTextInput>
   );
 };
