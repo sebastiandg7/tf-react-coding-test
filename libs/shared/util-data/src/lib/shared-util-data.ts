19f559b02d4c30e4eb2af241d7596a950fd13477
Sebastiandg7 <sebastiandg7@gmail.com>
2020-03-18 15:24:30 -0500

    fix(util-data): fix multiple sort property comparison for numbers

--- libs/shared/util-data/src/lib/shared-util-data.ts
+++ libs/shared/util-data/src/lib/shared-util-data.ts
@@ -1,27 +1,42 @@
-export interface MultipleSortParam {
-  property: string;
+export interface MultipleSortParam<K> {
+  property: keyof K;
   direction: number;
 }
 
-export const multipleSortArray = (
-  array: any[],
-  sortBy: MultipleSortParam[]
-) => {
-  return array.slice().sort((objectA, objectB) => {
+export function multipleSortArray<T>(
+  array: T[],
+  sortBy: MultipleSortParam<T>[]
+): T[] {
+  const arrayCopy = array.slice();
+  arrayCopy.sort((objectA, objectB) => {
     let i = 0;
     let result = 0;
     while (i < sortBy.length && result === 0) {
       result =
         sortBy[i].direction *
-        (objectA[sortBy[i].property].toString() <
-        objectB[sortBy[i].property].toString()
-          ? -1
-          : objectA[sortBy[i].property].toString() >
-            objectB[sortBy[i].property].toString()
-          ? 1
-          : 0);
+        compareValues(objectA[sortBy[i].property], objectB[sortBy[i].property]);
       i++;
     }
     return result;
   });
+  return arrayCopy;
+}
+
+export const compareValues = (valueA: any, valueB: any): number => {
+  let result = 0;
+
+  if (typeof valueA === 'number' && typeof valueB === 'number') {
+    const numberA = Number(valueA);
+    const numberB = Number(valueB);
+    result = numberA < numberB ? -1 : numberA > numberB ? 1 : 0;
+  } else {
+    result =
+      valueA.toString() < valueB.toString()
+        ? -1
+        : valueA.toString() > valueB.toString()
+        ? 1
+        : 0;
+  }
+
+  return result;
 };
