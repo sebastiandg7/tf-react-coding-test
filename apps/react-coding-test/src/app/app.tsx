1c0d0ef519b99451073a3aea0b62742866652259
Sebastian Duque <sebastiandg7@gmail.com>
2020-03-22 00:23:08 -0500

    feat(coding-test): fix register-form route match type

--- apps/react-coding-test/src/app/app.tsx
+++ apps/react-coding-test/src/app/app.tsx
@@ -63,7 +63,6 @@ export const App = () => {
             />
             <Route
               path="/register-form"
-              exact
               component={ReactCodingTestFeatureRegsiterForm}
             />
             <Route path="/voting-list" exact component={RandomVotingList} />
