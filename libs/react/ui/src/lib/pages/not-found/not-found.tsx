4fd2dcaafa6a7f78adbf62467ddc38b1fe71115c
Sebastian Duque <sebastiandg7@gmail.com>
2020-03-22 00:30:55 -0500

    style 404 page

--- libs/react/ui/src/lib/pages/not-found/not-found.tsx
+++ libs/react/ui/src/lib/pages/not-found/not-found.tsx
@@ -6,13 +6,27 @@ import styled from 'styled-components';
 export interface NotFoundProps {}
 
 const StyledNotFound = styled.div`
-  color: pink;
+  text-align: center;
+
+  h1 {
+    font-weight: bold;
+    font-size: 2rem;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  h1, h2 {
+    margin-top: 2em;
+  }
 `;
 
 export const NotFound = (props: NotFoundProps) => {
   return (
     <StyledNotFound>
-      <h1>Welcome to not-found component!</h1>
+      <h1>Oops...</h1>
+      <h2>The page you are looking for doesn't exists :(</h2>
     </StyledNotFound>
   );
 };
